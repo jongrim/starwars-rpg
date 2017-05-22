ca19f4e6adc726cc6178135bae3daf474b9f7da2
Jonathan Grim <jonjongrim@gmail.com>
2017-05-22 14:29:03 -0400

    Initial gameplay mechanics finished

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,111 @@
 // @ts-check
+
+$(document).ready(function () {
+  const luke = $("#luke");
+  const obiWan = $("#obi-wan");
+  const sidious = $("#sidious");
+  const vader = $("#vader");
+
+  let attackerDiv = $("#attacker");
+  let defenderDiv = $("#defender");
+
+  let attacker = null;
+  let defender = null;
+
+  const attackButton = $("#atk-btn");
+  const resetButton = $("#reset-btn");
+
+  attackButton.click(attack);
+  resetButton.click(reset);
+
+  
+  function allowCharacterSelection() {
+    $(".character").on("click", function () {
+      $(`#${this.id}`).remove();
+      addCharacterToBattle(event);
+    });
+  }
+
+  function addCharacterToBattle(event) {
+
+    if (!attacker) {
+      attackerDiv.append(event.currentTarget.outerHTML);
+      attacker = {
+        id: event.currentTarget.id,
+        attackPower: event.currentTarget.dataset.attack,
+        health: event.currentTarget.dataset.health,
+        attacks: 1,
+        attack: function () {
+          return this.attacks * this.attackPower;
+        }
+      }
+    } else {
+      defenderDiv.append(event.currentTarget.outerHTML);
+      defender = {
+        id: event.currentTarget.id,
+        attack: event.currentTarget.dataset.counterattack,
+        health: event.currentTarget.dataset.health,
+      }
+      $(".character").off("click");
+      prepareBattle();
+    }
+  }
+
+  function prepareBattle() {
+    attackButton.removeAttr('disabled');
+    resetButton.removeAttr('disabled');
+  }
+
+  function attack() {
+    doDamage();
+    attacker.attacks++;
+    if (attacker.health < 1) {
+      endGame();
+    }
+    if (defender.health < 1) {
+      removeCharacter(defender);
+    }
+    rewriteStats();
+  }
+
+  function doDamage() {
+    attacker.health -= defender.attack;
+    defender.health -= attacker.attack();
+  }
+
+  function removeCharacter(obj) {
+    $(`#${obj.id}`).remove();
+    defender = null;
+    allowCharacterSelection();
+    attackButton.attr('disabled', 'disabled');
+    if ($('#characterRow').find('.character').length < 1) {
+      endGame(true);
+    }
+  }
+
+  function rewriteStats() {
+    writeHealthStat(attackerDiv, attacker);
+    writeHealthStat(defenderDiv, defender);
+    attackerDiv.find(".attack").text(attacker.attack());
+  }
+
+  function writeHealthStat(div, obj) {
+    div.find(".health").text(() => {
+      return obj.health > 0 ? obj.health : 0;
+    });
+  }
+
+  function reset() {
+
+  }
+
+  function endGame(success) {
+    if (success) {
+      alert('You won');
+    } else {
+      alert('You lost');
+    }
+  }
+
+  allowCharacterSelection();
+})
